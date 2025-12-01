0747be2c39972d1f787d3977f045923d2d57a19e
M-Eran-Hasareli <eran.hasarali.2001@gmail.com>
2025-12-01 23:20:19 +0530

    add blade extension for new pages

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Home page
 Route::get('/', function () {
     return view('welcome');
 });
 
+// About page
 Route::get('/about', function () {
     return view('welcome');
 });
 
+// Contact page
+Route::get('/contact', function () {
+    return view('contact');
+});
