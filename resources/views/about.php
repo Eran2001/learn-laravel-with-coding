594f00e2325afaca5e87b04ce3225a9f6c1e805e
M-Eran-Hasareli <eran.hasarali.2001@gmail.com>
2025-12-01 22:48:56 +0530

    add about page

---
+++ resources/views/about.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About Page</title>
+</head>
+<body class="bg-black flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <h1 class="text-5xl text-white font-semibold">Hello from About page</h1>
+</body>
+</html>
